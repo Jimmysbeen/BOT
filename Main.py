a4adeded58f13dc2365ffd88fea33b69125a1c5d
Jimmysbeen <141806254+Jimmysbeen@users.noreply.github.com>
2023-08-23 09:45:20 +1200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 import requests as request
+import discord
+
+client = discord.client
 
 env:
   Token: ${{ secrets.Token }}
 
+@client.event
+async def on_ready():
+  print("I'm online!".format(client))
 
